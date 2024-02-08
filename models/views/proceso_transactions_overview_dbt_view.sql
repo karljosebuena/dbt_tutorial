66ef7b61647f7389d299a5709662f949defd65ec
Karl Jose Buena <karl.buena@cover.tech>
2024-02-08 19:04:32 +0800

    Testing query for the pie graph in preset

--- models/views/proceso_transactions_overview_dbt_view.sql
+++ models/views/proceso_transactions_overview_dbt_view.sql
@@ -5,9 +5,12 @@ WITH CTE AS (
     SUM(CAST(issues AS INT64)) AS issues_sum
   FROM
     `development-395907.dbt_demo_bigquery.proceso_joined_posthog_sentry_events_dbt_view`
+),
+IndividualRows AS (
+  SELECT 'Total Transactions' AS metric, total_transactions_sum AS value FROM CTE
+  UNION ALL
+  SELECT 'Success' AS metric, success_sum AS value FROM CTE
+  UNION ALL
+  SELECT 'Issues' AS metric, issues_sum AS value FROM CTE
 )
-SELECT
-*
-  -- CONCAT(CAST(ROUND(CAST((CTE.success_sum/CTE.total_transactions_sum) * 100 AS FLOAT64), 0) AS STRING), '%') as success_percentage,
-  -- CONCAT(CAST(ROUND(CAST((CTE.issues_sum/CTE.total_transactions_sum) * 100 AS FLOAT64), 0) AS STRING), '%') as issues_percentage
-FROM CTE
+SELECT * FROM IndividualRows;
