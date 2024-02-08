920bb42578108441383989b4ce0f607687b36e71
Karl Jose Buena <karl.buena@cover.tech>
2024-02-08 23:13:27 +0800

    No need for total transactions

--- models/views/proceso_transactions_overview_dbt_view.sql
+++ models/views/proceso_transactions_overview_dbt_view.sql
@@ -7,8 +7,6 @@ WITH CTE AS (
     `development-395907.dbt_demo_bigquery.proceso_joined_posthog_sentry_events_dbt_view`
 ),
 IndividualRows AS (
-  SELECT 'Total Transactions' AS metric, total_transactions_sum AS value FROM CTE
-  UNION ALL
   SELECT 'Success' AS metric, success_sum AS value FROM CTE
   UNION ALL
   SELECT 'Issues' AS metric, issues_sum AS value FROM CTE
