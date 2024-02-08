cbec5220231966d29dd5217b6ae7d3c42235ae8d
Karl Jose Buena <karl.buena@cover.tech>
2024-02-08 16:49:47 +0800

    Testing query for the pie graph in preset

--- models/views/proceso_transactions_overview_dbt_view.sql
+++ models/views/proceso_transactions_overview_dbt_view.sql
@@ -7,6 +7,7 @@ WITH CTE AS (
     `development-395907.dbt_demo_bigquery.proceso_joined_posthog_sentry_events_dbt_view`
 )
 SELECT
-  CONCAT(CAST(ROUND(CAST((CTE.success_sum/CTE.total_transactions_sum) * 100 AS FLOAT64), 0) AS STRING), '%') as success_percentage,
-  CONCAT(CAST(ROUND(CAST((CTE.issues_sum/CTE.total_transactions_sum) * 100 AS FLOAT64), 0) AS STRING), '%') as issues_percentage
+*
+  -- CONCAT(CAST(ROUND(CAST((CTE.success_sum/CTE.total_transactions_sum) * 100 AS FLOAT64), 0) AS STRING), '%') as success_percentage,
+  -- CONCAT(CAST(ROUND(CAST((CTE.issues_sum/CTE.total_transactions_sum) * 100 AS FLOAT64), 0) AS STRING), '%') as issues_percentage
 FROM CTE
