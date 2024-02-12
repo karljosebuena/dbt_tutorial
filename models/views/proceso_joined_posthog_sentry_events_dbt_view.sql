4bc3960b9137ccc4e838251d77e5f4d2f46a64ea
Karl Jose Buena <karl.buena@cover.tech>
2024-02-12 14:10:54 +0800

    Feat: PROCESO-512 - Testing querries for preset charts

--- models/views/proceso_joined_posthog_sentry_events_dbt_view.sql
+++ models/views/proceso_joined_posthog_sentry_events_dbt_view.sql
@@ -1,11 +1,10 @@
 SELECT
-  pV.full_url,
-  UPPER(SUBSTR(pV.current_url, 2, 1)) || LOWER(SUBSTR(pV.current_url, 3)) AS operation,
-  pV.row_count as total_transactions,
-  pV.row_count - sV.row_count as success,
-  sV.row_count as issues,
-  FORMAT_TIMESTAMP('%b %e, %Y %I:%M %p', pV.last_transaction) as last_transaction,
-  pV.last_transaction as raw_last_transaction
+  pV.full_url as `URL`,
+  UPPER(SUBSTR(pV.current_url, 2, 1)) || LOWER(SUBSTR(pV.current_url, 3)) AS `Operation`,
+  pV.row_count as `Total Transactions`,
+  pV.row_count - sV.row_count as `Success`,
+  sV.row_count as `Issues`,
+  FORMAT_TIMESTAMP('%b %e, %Y %I:%M %p', pV.last_transaction) as `Last Transaction`
  FROM `development-395907.dbt_demo_bigquery.proceso_posthog_events_dbt_view` pV
   INNER JOIN `development-395907.dbt_demo_bigquery.proceso_sentry_events_dbt_view` sV
-    ON pV.full_url = sV.full_url
+    ON pV.full_url = sV.full_url;
