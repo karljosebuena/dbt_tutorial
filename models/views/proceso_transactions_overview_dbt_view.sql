194d2776504eebe2166734e56f16a60bc1493126
Karl Jose Buena <karl.buena@cover.tech>
2024-02-12 14:24:18 +0800

    Feat: PROCESO-512 - Testing querries for preset charts

--- models/views/proceso_transactions_overview_dbt_view.sql
+++ models/views/proceso_transactions_overview_dbt_view.sql
@@ -1,14 +1,12 @@
-WITH CTE AS (
+WITH PROCESO_TRANSACTIONS_OVERVIEW_CTE AS (
   SELECT
-    SUM(CAST(total_transactions AS INT64)) AS total_transactions_sum,
-    SUM(CAST(success AS INT64)) AS success_sum,
-    SUM(CAST(issues AS INT64)) AS issues_sum
+    SUM(CAST(`Total Transactions` AS INT64)) AS total_transactions_sum,
+    SUM(CAST(`SUCCESS` AS INT64)) AS success_sum,
+    SUM(CAST(`Issues` AS INT64)) AS issues_sum
   FROM
     `development-395907.dbt_demo_bigquery.proceso_joined_posthog_sentry_events_dbt_view`
-),
-IndividualRows AS (
-  SELECT 'Success' AS metric, success_sum AS value FROM CTE
-  UNION ALL
-  SELECT 'Issues' AS metric, issues_sum AS value FROM CTE
 )
-SELECT * FROM IndividualRows
+SELECT
+  success_sum as `SUCCESS`,
+  issues_sum as `Issues`
+FROM PROCESO_TRANSACTIONS_OVERVIEW_CTE
