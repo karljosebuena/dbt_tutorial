a1d7148dfb0ec34cdd68320d0c210392488738a6
Karl Jose Buena <karl.buena@cover.tech>
2024-02-15 12:57:23 +0800

    Feat: PROCESO-512 - Testing inserting anchor tag to preset table

--- models/views/proceso_joined_posthog_sentry_events_dbt_view.sql
+++ models/views/proceso_joined_posthog_sentry_events_dbt_view.sql
@@ -1,10 +1,14 @@
 SELECT
-  pV.full_url as `URL`,
+  pV.full_url AS `URL`,
   UPPER(SUBSTR(pV.current_url, 2, 1)) || LOWER(SUBSTR(pV.current_url, 3)) AS `Operation`,
-  pV.row_count as `Total Transactions`,
-  pV.row_count - sV.row_count as `Success`,
-  sV.row_count as `Issues`,
-  pV.last_transaction as `Last Transaction`
- FROM `development-395907.dbt_demo_bigquery.proceso_posthog_events_dbt_view` pV
-  INNER JOIN `development-395907.dbt_demo_bigquery.proceso_sentry_events_dbt_view` sV
-    ON pV.full_url = sV.full_url
+  pV.row_count AS `Total Transactions`,
+  pV.row_count - sV.row_count AS `Success`,
+  sV.row_count AS `Issues`,
+  pV.last_transaction AS `Last Transaction`,
+  CONCAT("<a href='https://covertech-873987ba3.sentry.io/issues/?environment=development&project=4505878431268864&query=is%3Aunresolved+url%3A%22https%3A%2F%2Fdevelopment-395907.web.app%2Fquotes%22&referrer=issue-list&statsPeriod=30d'>View Issues</a>") AS `View Issues`
+FROM
+  `development-395907.dbt_demo_bigquery.proceso_posthog_events_dbt_view` pV
+INNER JOIN
+  `development-395907.dbt_demo_bigquery.proceso_sentry_events_dbt_view` sV
+ON
+  pV.full_url = sV.full_url
