0e2f5cf3b5462fdae5cfe1927dbb486643200286
Karl Jose Buena <karl.buena@cover.tech>
2024-03-06 12:20:27 +0800

    Feat: PLAT-4 - Updated proceso_posthog_events_dbt_model schema

--- models/proceso_posthog_events_dbt_model.sql
+++ models/proceso_posthog_events_dbt_model.sql
@@ -9,21 +9,30 @@ WITH EventData AS (
     _airbyte_raw_id,
     id,
     event,
-    JSON_EXTRACT_SCALAR(properties, "$['$session_id']") AS session_id,
+    -- JSON_EXTRACT_SCALAR(properties, "$['$session_id']") AS session_id,
     distinct_id,
-    JSON_EXTRACT_SCALAR(properties, "$['$host']") AS host,
-    JSON_EXTRACT_SCALAR(properties, "$['$current_url']") AS current_url,
-    JSON_EXTRACT_SCALAR(properties, "$['$initial_current_url']") AS initial_current_url,
-    JSON_EXTRACT_SCALAR(person, "$.properties.name") AS user_name,
-    JSON_EXTRACT_SCALAR(person, "$.properties.email") AS user_email,
+    JSON_EXTRACT_SCALAR(properties, '$.req.protocol') AS protocol,
+    JSON_EXTRACT_SCALAR(properties, '$.req.hostname') AS hostname,
+    JSON_EXTRACT_SCALAR(properties, '$.req.baseUrl') AS baseUrl,
+    JSON_EXTRACT_SCALAR(properties, '$.req.path') AS path,
+    JSON_EXTRACT_SCALAR(properties, '$.req.originalUrl') AS originalUrl,
+    JSON_EXTRACT_SCALAR(properties, '$.req.fullUrl') AS fullUrl,
+    JSON_EXTRACT_SCALAR(properties, '$.res.statusCode') AS statusCode,
+    JSON_EXTRACT_SCALAR(properties, '$.res.data') AS data,
+    JSON_EXTRACT_SCALAR(person, '$.properties.name') AS user_name,
+    JSON_EXTRACT_SCALAR(person, '$.properties.email') AS user_email,
     FORMAT_TIMESTAMP('%Y-%m-%d %I:%M:%S %p', timestamp) as date_created
   FROM
     `development-395907.posthog_airbyte_bigquery_sync.events`
   WHERE
-    TIMESTAMP_TRUNC(timestamp, DAY) >= TIMESTAMP("2024-02-01")
-    -- AND event = '$pageview' -- remove this filter
-    AND JSON_EXTRACT_SCALAR(properties, "$['$host']") = "development-395907.web.app"
+    TIMESTAMP_TRUNC(timestamp, DAY) >= TIMESTAMP("2024-01-01")
+    AND event = 'Proceso Backend'
+    -- AND JSON_EXTRACT_SCALAR(properties, '$.req.hostname') = 'development-395907.web.app'
+    AND (
+      JSON_EXTRACT_SCALAR(properties, '$.req.hostname') = 'localhost'
+      OR JSON_EXTRACT_SCALAR(properties, '$.req.hostname') = '127.0.0.1'
+    )
 )
 SELECT *
 FROM EventData
-ORDER BY date_created, session_id
+ORDER BY date_created
