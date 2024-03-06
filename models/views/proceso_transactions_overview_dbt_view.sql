50be4c74d0c9a7ea384793f3bc70b038a6775555
Karl Jose Buena <karl.buena@cover.tech>
2024-03-06 14:59:01 +0800

    feat: source values for proceso_transactions_overview_dbt_view

--- models/views/proceso_transactions_overview_dbt_view.sql
+++ models/views/proceso_transactions_overview_dbt_view.sql
@@ -1,10 +1,10 @@
 WITH PROCESO_TRANSACTIONS_OVERVIEW_CTE AS (
   SELECT
-    SUM(CAST(`Total Transactions` AS INT64)) AS total_transactions_sum,
-    SUM(CAST(`Success` AS INT64)) AS success_sum,
-    SUM(CAST(`Issues` AS INT64)) AS issues_sum
+    SUM(total_transactions) AS total_transactions_sum,
+    SUM(success_count) AS success_sum,
+    SUM(fail_count) AS issues_sum
   FROM
-    `development-395907.dbt_demo_bigquery.proceso_joined_posthog_sentry_events_dbt_view`
+    `development-395907.dbt_demo_bigquery.proceso_posthog_events_success_fail_dbt_view`
 ),
 IndividualRows AS (
   SELECT 'Success' AS metric, success_sum AS value FROM PROCESO_TRANSACTIONS_OVERVIEW_CTE
