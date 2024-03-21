05c41d97a05f4339dffa30a9611547ec7c759e9d
Karl Jose Buena <karl.buena@cover.tech>
2024-03-21 15:23:08 +0800

    Added proceso_logs_success_fail_dbt_view

--- models/proceso_logs_dbt_model.sql
+++ models/proceso_logs_dbt_model.sql
@@ -7,8 +7,8 @@
 WITH EventData AS (
   SELECT
   timestamp,
-  JSON_EXTRACT_SCALAR(json_payload, '$.level') as logLevel,
-  JSON_EXTRACT_SCALAR(json_payload, '$.msg') as logMessage,
+  JSON_EXTRACT_SCALAR(json_payload, '$.level') as log_level,
+  JSON_EXTRACT_SCALAR(json_payload, '$.msg') as log_message,
   JSON_EXTRACT_SCALAR(json_payload, '$.req.headers.host') as host,
   JSON_EXTRACT_SCALAR(json_payload, '$.req.headers.x-cloud-trace-context') as cloud_trace_context,
   JSON_EXTRACT_SCALAR(json_payload, '$.req.headers.x-posthog-session-id') as posthog_session_id,
