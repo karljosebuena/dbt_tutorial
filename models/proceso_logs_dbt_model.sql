56b22f387e0a0d557b92883e337d904a3edb49fe
Karl Jose Buena <karl.buena@cover.tech>
2024-03-21 15:01:21 +0800

    Added proceso_logs_dbt_model

---
+++ models/proceso_logs_dbt_model.sql
@@ -0,0 +1,25 @@
+{{
+    config(
+        materialized='table'
+    )
+}}
+
+WITH EventData AS (
+  SELECT
+  timestamp,
+  JSON_EXTRACT_SCALAR(json_payload, '$.level') as logLevel,
+  JSON_EXTRACT_SCALAR(json_payload, '$.msg') as logMessage,
+  JSON_EXTRACT_SCALAR(json_payload, '$.req.headers.host') as host,
+  JSON_EXTRACT_SCALAR(json_payload, '$.req.headers.x-cloud-trace-context') as cloud_trace_context,
+  JSON_EXTRACT_SCALAR(json_payload, '$.req.headers.x-posthog-session-id') as posthog_session_id,
+  JSON_EXTRACT_SCALAR(json_payload, '$.res.statusCode') as status_code,
+  JSON_EXTRACT_SCALAR(json_payload, '$.responseTime') as response_time,
+  JSON_EXTRACT_SCALAR(json_payload, '$.req.url') as url,
+  FROM
+    `development-395907.proceso_logs._AllLogs`
+  WHERE
+    JSON_EXTRACT_SCALAR(json_payload, '$.req.headers.host') = 'procesoportalv2-5aa2zjftgq-ts.a.run.app'
+)
+SELECT *
+FROM EventData
+ORDER BY timestamp
