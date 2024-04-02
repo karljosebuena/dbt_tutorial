1457b885fe3a81daf841f0398367dcd47953abc3
Karl Jose Buena <karljosebuena@gmail.com>
2024-04-02 18:30:20 +0800

    Open new tab when action is clicked

--- models/proceso_logs/views/coverai_logs_success_fail_dbt_view.sql
+++ models/proceso_logs/views/coverai_logs_success_fail_dbt_view.sql
@@ -29,6 +29,6 @@ SELECT
   COALESCE(SUCCESS_CTE.row_count, 0) AS `success_count`,
   COALESCE(FAIL_CTE.row_count, 0) AS `fail_count`,
   COALESCE(SUCCESS_CTE.last_transaction, FAIL_CTE.last_transaction) AS last_transaction,
-  CONCAT("<a href='https://covertech-873987ba3.sentry.io/issues/?environment=dev&project=4505878638559232&query=is%3Aunresolved+url%3A%22", "*", COALESCE(SUCCESS_CTE.host, FAIL_CTE.host) || "/" || COALESCE(SUCCESS_CTE.route, FAIL_CTE.route), "*", "%22&referrer=issue-list&statsPeriod=30d'>View Issues</a>") AS `view_issues`
+  CONCAT("<a target='_blank' rel='noopener noreferrer' href='https://covertech-873987ba3.sentry.io/issues/?environment=dev&project=4505878638559232&query=is%3Aunresolved+url%3A%22", "*", COALESCE(SUCCESS_CTE.host, FAIL_CTE.host) || "/" || COALESCE(SUCCESS_CTE.route, FAIL_CTE.route), "*", "%22&referrer=issue-list&statsPeriod=30d>View Issues</a>") AS `view_issues`
 FROM SUCCESS_CTE
 FULL OUTER JOIN FAIL_CTE ON SUCCESS_CTE.route = FAIL_CTE.route
